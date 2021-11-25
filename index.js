1964af41c99c66b4791db61cfc7ccf55ae77bc1c
Wendell Santos <wendellsantooos@gmail.com>
2021-11-25 11:06:04 -0300

    Css estruturado e JS iniciado

---
+++ index.js
@@ -0,0 +1,10 @@
+function verificar() {
+var nome = document.getElementById("pessoa")
+var idade = document.getElementById("idade")
+var cep = document.getElementById("cep")
+var resposta =
+
+if (idade > 110 || idade <= 18) {
+
+}
+}
